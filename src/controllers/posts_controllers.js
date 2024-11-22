2d7708b7db6fa8f5608b1e41b9f0157bc1ff41e3
Guilherme de Oliveira Batista <150380352+guioliveirx@users.noreply.github.com>
2024-11-22 00:17:51 -0300

    feat: create new upload route and upload_image function

--- src/controllers/posts_controllers.js
+++ src/controllers/posts_controllers.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { create_post, get_all_posts } from "../models/posts_model.js";
 
 export async function list_posts (req, res){
@@ -10,6 +11,24 @@ export async function create_new_post(req, res){
 
     try {
         const post_created = await create_post(new_post);
+        res.status(201).json(post_created);
+    } catch(error) {
+        console.error(error.message);
+        res.status(500).json({"Erro": "Falha na requisição"});
+    }
+}
+
+export async function upload_image(req, res){
+    const new_post = {
+        descricao: "",
+        img_url: req.file.originalmente,
+        alt: ""
+    }
+
+    try {
+        const post_created = await create_post(new_post);
+        const image_updated = `uploads/${post_created.insertedId}.png`;
+        fs.renameSync(req.file.path, image_updated);
         res.status(201).json(post_created);
     } catch(error) {
         console.error(error.message);
--- src/routes/posts_routs.js
+++ src/routes/posts_routs.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import multer from "multer";
-import { create_new_post, list_posts } from '../controllers/posts_controllers.js';
+import { create_new_post, list_posts, upload_image } from '../controllers/posts_controllers.js';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use(express.json());
@@ -8,6 +19,8 @@ const routes = (app) => {
     app.get("/posts", list_posts);   
     // Rota para criar um novo post
     app.post("/posts", create_new_post);
+
+    app.post("/upload", upload.single("image"), upload_image);
 };
 
 export default routes;
